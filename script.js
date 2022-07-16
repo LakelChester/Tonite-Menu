cd498e6e39c553c387e40e039a97ab80ac6ed0ce
LakelChester <lakel@chesterassociate.com>
2022-07-15 17:31:51 -0700

    add event listner for html form

--- script.js
+++ script.js
@@ -36,6 +36,28 @@ function getAllRecipes(){
 
 }
 
+const init = () => {
+    const inputForm = document.querySelector('form')
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=e3fd2432ac2e4170a1303b4f11d6d026${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        const title = document.querySelector('section#recipePicture h4');
+        const summary = document.querySelector('section#recipePicture p');
+  
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
+
+
 //const getRecipes = async () => {
 //    const recipeQuery = inputField.value;
 //    const endpoint = `${url}${queryParams}${recipeQuery}`;
